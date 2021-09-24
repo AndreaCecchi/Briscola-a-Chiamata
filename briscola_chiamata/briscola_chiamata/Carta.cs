a1e513d0993d046604eb3f33b5a39a7c5086c88f
GiuliaFas <86486192+GiuliaFas@users.noreply.github.com>
2021-09-24 12:19:51 +0200

    Add files via upload

---
+++ briscola_chiamata/briscola_chiamata/Carta.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace briscola_chiamata
+{
+    class Carta
+    {
+        public string figura;
+        public string seme;
+        public int valore;
+
+        public Carta(string aFigura, string aSeme)
+        {
+            figura = aFigura;
+            seme = aSeme;
+            if (figura == "asso")
+                valore = 11;
+            if (figura == "tre")
+                valore = 10;
+            if (figura == "re")
+                valore = 4;
+            if (figura == "donna")
+                valore = 3;
+            if (figura == "fante")
+                valore = 2;
+            else
+                valore = 0;
+        }
+    }
+}
---
+++ briscola_chiamata/briscola_chiamata/Form1.Designer.cs
@@ -0,0 +1,52 @@
+﻿
+namespace briscola_chiamata
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(700, 422);
+            this.Font = new System.Drawing.Font("Roboto", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Briscola";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    
+    }
+}
+
